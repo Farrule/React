9c8de14d05487b825852d801d75490fcffaff0c7
Farrule <tsukasap0921@gmail.com>
2022-07-27 10:45:52 +0900

    Judge

--- sanmoku-narabe/src/components/Board.tsx
+++ sanmoku-narabe/src/components/Board.tsx
@@ -3,14 +3,48 @@ import '../styles/board.css';
 import { Square } from './Square';
 
 export const Board = () => {
-  const status: string = 'Next player : X';
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+
   const handleClick = (i: number) => {
     const _squares = squares.slice();
-    _squares[i] = 'X';
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    _squares[i] = xIsNext ? 'X' : 'O';
     setSquares(_squares);
+    setXIsNext(!xIsNext);
   }
+
   const renderSquare = (i: number) => <Square value={squares[i]} onClick={() => handleClick(i)}/>
+
+  const calculateWinner = (squares: Array<number>) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const winner = calculateWinner(squares);
+  let status: string;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
 
   return (
     <div>
